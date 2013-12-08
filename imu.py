a114ca9a565ae0012963d9be180a81a3ebc92de9
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-08 22:09:49 +0100

    debug log

--- imu.py
+++ imu.py
@@ -45,6 +45,9 @@ class IMU():
         self.gyr_ba = array([0.,0.,0.])
         self.tcurrent = 0.
         self.deltat = 0.
+        self.acc = (0.,0.,0.)
+        self.gyr = (0.,0.,0.)
+        self.mag = (0.,0.,0.)
 
         self.start()
 
