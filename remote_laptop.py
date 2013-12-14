dbd157b802f30dfc9af6431ba73b83aee04c3cb3
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-14 16:43:59 +0100

    remote

--- remote_laptop.py
+++ remote_laptop.py
@@ -1,6 +1,7 @@
 import Pyro4
 
-uri=raw_input("What is the Pyro uri of the greeting object? ").strip()
+#uri=raw_input("What is the Pyro uri of the greeting object? ").strip()
 
-imu=Pyro4.Proxy(uri)          # get a Pyro proxy to the greeting object
+imu=Pyro4.Proxy("PYRONAME:r_IMU")          # get a Pyro proxy to the greeting object
+
 print imu.getEuler()          # call method normally
