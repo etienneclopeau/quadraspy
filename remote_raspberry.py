37b94bce299dc3929153e06ff1a5f0deaa8ea05b
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-14 16:43:59 +0100

    remote

--- remote_raspberry.py
+++ remote_raspberry.py
@@ -4,7 +4,9 @@ from imu import IMU
 imu=IMU()
 
 daemon=Pyro4.Daemon()                 # make a Pyro daemon
+ns=Pyro4.locateNS()                   # find the name server
 uri=daemon.register(imu)   # register the greeting object as a Pyro object
+ns.register("r_IMU", uri)  # register the object with a name in the name server
 
 print "Ready. Object uri =", uri      # print the uri so we can use it in the client later
 daemon.requestLoop()  
