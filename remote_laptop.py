4b0a0cd62ddade3ef4de8bd5253f9e879b177b44
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-14 22:24:30 +0100

    remote

--- remote_laptop.py
+++ remote_laptop.py
@@ -2,6 +2,20 @@ import Pyro4
 
 #uri=raw_input("What is the Pyro uri of the greeting object? ").strip()
 
+#-- Initialize Pyro client
+import Pyro4.naming, Pyro4.core, sys
+#Pyro4.core.initClient()
+
+#-- Locate the Name Server
+print 'Searching Name Server...'
+
+ns = Pyro4.locateNS(host = 'raspberrypi', port = 9090)
+print 'Name Server found at %s (%s) port %s' % \
+ ( ns.URI.address,
+ Pyro4.protocol.getHostname(ns.URI.address) or '??',
+ ns.URI.port )
+
+
 imu=Pyro4.Proxy("PYRONAME:r_IMU")          # get a Pyro proxy to the greeting object
 
 print imu.getEuler()          # call method normally
--- remote_raspberry.py
+++ remote_raspberry.py
@@ -3,7 +3,9 @@ from imu import IMU
 
 imu=IMU()
 
-daemon=Pyro4.Daemon()                 # make a Pyro daemon
+#Pyro4.naming.startNS(host=None, port=None, enableBroadcast=True, bchost=None, bcport=None, unixsocket=None, nathost=None, natport=None)
+
+daemon=Pyro4.Daemon(host="192.168.1.98")                 # make a Pyro daemon
 ns=Pyro4.locateNS()                   # find the name server
 uri=daemon.register(imu)   # register the greeting object as a Pyro object
 ns.register("r_IMU", uri)  # register the object with a name in the name server
