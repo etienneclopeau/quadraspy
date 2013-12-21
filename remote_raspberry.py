efadc37459e5eaf52e1ee47d5cbac8006719582c
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-21 21:48:47 +0100

    remote

--- remote_raspberry.py
+++ remote_raspberry.py
@@ -8,7 +8,8 @@ imu=IMU()
 
 #Pyro4.naming.startNS(host=None, port=None, enableBroadcast=True, bchost=None, bcport=None, unixsocket=None, nathost=None, natport=None)
 
-daemon=Pyro4.Daemon(host="192.168.1.98")                 # make a Pyro daemon
+# daemon=Pyro4.Daemon(host="192.168.1.98")                 # make a Pyro daemon
+daemon=Pyro4.Daemon(host="192.168.1.28")                 # make a Pyro daemon
 ns=Pyro4.locateNS()                   # find the name server
 uri=daemon.register(imu)   # register the greeting object as a Pyro object
 ns.register("r_IMU", uri)  # register the object with a name in the name server
