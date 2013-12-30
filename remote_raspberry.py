44c33cfd9ed037cbe91189e6d9fe066230ea6cbc
Clopeau Etienne <Clopeau Etienne@localhost>
2013-12-30 15:23:00 +0100

    remote en pickle

--- remote_raspberry.py
+++ remote_raspberry.py
@@ -1,5 +1,7 @@
 
-#lancer: python -m Pyro4.naming -n 192.168.1.98
+#lancer: 
+ # set PYRO_SERIALIZER=pickle
+ # python -m Pyro4.naming -n 192.168.1.98
 
 import Pyro4
 from imu import IMU
@@ -8,6 +10,7 @@ imu=IMU()
 
 #Pyro4.naming.startNS(host=None, port=None, enableBroadcast=True, bchost=None, bcport=None, unixsocket=None, nathost=None, natport=None)
 Pyro4.config.SERIALIZERS_ACCEPTED.add('pickle')
+Pyro4.config.SERIALIZER = 'pickle'
 daemon=Pyro4.Daemon(host="192.168.1.98")                 # make a Pyro daemon
 # daemon=Pyro4.Daemon(host="192.168.1.28")                 # make a Pyro daemon
 ns=Pyro4.locateNS()                   # find the name server
