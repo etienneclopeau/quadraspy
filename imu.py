777fc6d571e4c8c26c35e1bd53684dfe360494a3
Clopeau Etienne <Clopeau Etienne@localhost>
2014-01-03 14:40:27 +0100

    imu

--- imu.py
+++ imu.py
@@ -122,8 +122,8 @@ class IMU():
         self.tbefore = self.tcurrent
         self.tcurrent = time()
         self.deltat = self.tcurrent - self.tbefore
-        self.acc = 0.9 * self.acc + 0.1 * self.accelerometer.getAcc()
-        self.gyr = 0.9 * self.gyr + 0.1 * self.gyrometer.getGyr()
+        self.acc = 0.5 * self.acc + 0.5 * self.accelerometer.getAcc()
+        self.gyr = 0.5 * self.gyr + 0.5 * self.gyrometer.getGyr()
         self.mag = self.magnetometer.getMag()
 
 
